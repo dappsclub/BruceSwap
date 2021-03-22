1f25c4b2789188a20d7ed20fea30ee9661511ff9
dappsclub <cadillac.bupt@qq.com>
2021-03-22 23:58:55 +0800

    修改一些参数

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -3,7 +3,7 @@ import { AbstractConnector } from '@web3-react/abstract-connector'
 
 import { fortmatic, injected, portis, walletconnect, walletlink } from '../connectors'
 
-export const ROUTER_ADDRESS = '0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D'
+export const ROUTER_ADDRESS = '0x4e27ED665111Fa36C49369708CF0C1D30574cF57'//ropsten
 
 export const ZERO_ADDRESS = '0x0000000000000000000000000000000000000000'
 
